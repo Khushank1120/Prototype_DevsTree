ee082a62877163ffa074b77a5a51559cc78d9ce8
khushank <mistry.khushank@gmail.com>
2023-01-24 21:30:14 +0530

    Map Activity-3

--- app/src/main/java/com/example/prototype/MapActivity.java
+++ app/src/main/java/com/example/prototype/MapActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
 import android.widget.EditText;
@@ -18,10 +20,14 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private MapView mapView;
     private GoogleMap mMap;
+    SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +38,42 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
 
+        searchView = findViewById(R.id.searchView);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                String location = searchView.getQuery().toString();
+                List<Address> addressList = null;
+
+                if(location!= null || !location.equals(""))
+                {
+                    Geocoder geocoder = new Geocoder((MapActivity.this));
+                    try {
+                        addressList = geocoder.getFromLocationName(location,1);
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(),address.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,16));
+                }
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
 
     }
 
-    LatLng location = new LatLng(12.9716, 77.5946);
-    MarkerOptions markerOptions = new MarkerOptions().position(location).title("Bangalore");
+//    LatLng location = new LatLng(12.9716, 77.5946);
+//    MarkerOptions markerOptions = new MarkerOptions().position(location).title("Bengaluru");
 
 
     @Override
@@ -47,8 +84,9 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         mMap.getUiSettings().setMyLocationButtonEnabled(true);
         mMap.getUiSettings().setCompassEnabled(true);
         mMap.getUiSettings().setMapToolbarEnabled(true);
-        mMap.addMarker(markerOptions);
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 16f));
+
+//        mMap.addMarker(markerOptions);
+//        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(location, 16f));
 
     }
 
@@ -81,5 +119,4 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
     }
-
 }
